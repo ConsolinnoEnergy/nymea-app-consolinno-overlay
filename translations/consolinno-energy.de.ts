d07396cea6399146caa241bf2b6286f45bb0732b
IT20AI <anton-iljaschenko@gmx.de>
2025-05-19 15:41:43 +0200

    fix: complete German translations for alert messages in ConsolinnoGridSupportiveControl

--- translations/consolinno-energy.de.ts
+++ translations/consolinno-energy.de.ts
@@ -1093,11 +1093,11 @@
     <name>ConsolinnoGridSupportiveControlAlert</name>
     <message>
         <source>Attention</source>
-        <translation type="unfinished"></translation>
+        <translation>Achtung</translation>
     </message>
     <message>
         <source>Existing set-up will be overwritten.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestehende Einrichtungen werden überschrieben.</translation>
     </message>
 </context>
 <context>
