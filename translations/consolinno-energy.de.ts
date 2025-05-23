5fb7172be9021e919a765e948b0776a321bb72e8
IT20AI <anton-iljaschenko@gmx.de>
2025-05-23 09:35:49 +0200

    fix: layout adjustments in grid support views, updatet translations, adjust icon color

--- translations/consolinno-energy.de.ts
+++ translations/consolinno-energy.de.ts
@@ -3221,10 +3221,6 @@ fügen Sie eine weitere hinzu.</translation>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <source>The relays are configured as follows</source>
-        <translation>Die Relais sind wie folgt konfiguriert</translation>
-    </message>
-    <message>
         <source>Complete setup</source>
         <translation>Einrichtung abschliessen</translation>
     </message>
