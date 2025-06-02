5071eba8ad78d837c4bedb6459255da9050683fa
IT20AI <anton-iljaschenko@gmx.de>
2025-06-02 16:03:39 +0200

    feat: ESUI-369, ESUI-370 update battery charge messages and add header to dialog for zero compensation

--- translations/consolinno-energy.de.ts
+++ translations/consolinno-energy.de.ts
@@ -924,15 +924,15 @@
     </message>
     <message>
         <source>On days with negative electricity prices, battery capacity is actively retained so that the battery can be charged during hours with negative electricity prices and feed-in without compensation is avoided. As soon as the control becomes active, the charging of the battery is limited (visible by the yellow message on the screen.) The control is based on the forecast of PV production and household consumption and postpones charging accordingly:</source>
-        <translation type="unfinished">An Tagen mit negativen Strompreisen wird aktiv Batteriekapazität vorgehalten, um zu den Stunden mit negativen Strompreisen in die Batterie laden zu können und eine Einspeisung ohne Vergütung zu vermeiden. Sobald die Regelung aktiv wird, ist das Laden der Batterie  limitiert (sichtbar an der gelben Meldung im Screen.) Die Steuerung basiert auf der Vorhersage der PV-Produktion und  Hausverbrauch und verschiebt die Ladung entsprechend:</translation>
+        <translation>An Tagen mit negativen Strompreisen wird aktiv Batteriekapazität vorgehalten, um zu den Stunden mit negativen Strompreisen in die Batterie laden zu können und eine Einspeisung ohne Vergütung zu vermeiden. Sobald die Regelung aktiv wird, i st das Laden der Batterie  limitiert (sichtbar an der gelben Meldung im Screen.) Die Steuerung basiert auf der Vorhersage der PV-Produktion und  Hausverbrauch und verschiebt die Ladung entsprechend:</translation>
     </message>
     <message>
         <source>Battery charging is limited while the controller is active. &lt;u&gt;More Information&lt;/u&gt;</source>
         <translation>Wenn die Nullvergütung aktiv ist wird eine Meldung angezeigt. &lt;u&gt;Mehr Information &lt;/u&gt;</translation>
     </message>
     <message>
-        <source>Avoid zero compensation</source>
-        <translation type="unfinished">Nullvergütung vermeiden</translation>
+        <source>Zero compensation</source>
+        <translation>Nullvergütung</translation>
     </message>
 </context>
 <context>
