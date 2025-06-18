ba412be429db7bcf1ea3ee71a083e69ab064066c
IT20AI <anton-iljaschenko@gmx.de>
2025-06-18 15:02:02 +0200

    fix: update version number to 1.10.0 and modify change notification text in translations

--- translations/consolinno-energy.en.ts
+++ translations/consolinno-energy.en.ts
@@ -2727,11 +2727,10 @@ Use a timeout value greater or equal to 10 ms.</source>
     <message>
         <source>CHANGENOTIFICATION_PLACEHOLDER</source>
         <translation type="unfinished">&lt;h3&gt;The app has been updated.&lt;/h3&gt;
-&lt;p&gt;Version 1.9.2&lt;/p&gt;
-&lt;h4&gt;Improvements&lt;/h4&gt;
+&lt;p&gt;Version 1.10.0&lt;/p&gt;
+&lt;h4&gt;New&lt;/h4&gt;
 &lt;ul&gt;
-    &lt;li&gt;Indication and information text in case of avoiding zero compensation (specific control of battery charging for selected models)&lt;/li&gt;
-    &lt;li&gt;Message text for pending software update&lt;/li&gt;
+    &lt;li&gt;I&lt;/li&gt;
 &lt;/ul&gt;</translation>
     </message>
     <message>
@@ -2740,10 +2739,6 @@ Use a timeout value greater or equal to 10 ms.</source>
     </message>
     <message>
         <source>Email: &lt;a href=&apos;mailto:%1&apos;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Phone: &lt;a href=&apos;tel:%1&apos;&gt;%1&lt;/a&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
