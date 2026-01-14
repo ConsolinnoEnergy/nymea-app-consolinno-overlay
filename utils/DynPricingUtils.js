e93f2dd762198e61812e6e23516c07458a99db74
Michael Schwarz <m.schwarz@consolinno.de>
2026-01-14 12:42:12 +0100

    handle edge case

--- utils/DynPricingUtils.js
+++ utils/DynPricingUtils.js
@@ -7,8 +7,10 @@ function getVAT(dpThing) {
 
     let epexDayAheadThingClassId = "{678dd2a6-b162-4bfb-98cc-47f225f9008c}";
     if (dpThing.thingClass.id.toString() === epexDayAheadThingClassId) {
-        let vat = dpThing.paramByName("addedVAT").value;
-        return vat;
+        let addedVATParam = dpThing.paramByName("addedVAT");
+        return (addedVATParam && typeof addedVATParam.value === "number") ?
+                    addedVATParam.value :
+                    0.;
     } else {
         // Other dynamic tariffs already include the VAT in their prices.
         return 0.;
