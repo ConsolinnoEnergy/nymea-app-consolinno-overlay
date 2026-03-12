4b05de744c57b419d33f8624deb3b89c63c230a1
Oliver Wieland <o.wieland@consolinno.de>
2026-03-12 08:45:52 +0100

    Revert ESUI-831 Renamed "Fahrzeug-Laden aus Batterie sperren" to "Fahrzeug nicht aus Batterie laden

--- translations/consolinno-energy.de.ts
+++ translations/consolinno-energy.de.ts
@@ -301,7 +301,7 @@
     </message>
     <message>
         <source>Block EV charging from the battery</source>
-        <translation>Fahrzeug-Laden aus Batterie sperren</translation>
+        <translation>Fahrzeug nicht aus Batterie laden</translation>
     </message>
 </context>
 <context>
