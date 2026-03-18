be4b00bf63f95434312ae5cdf98159f0a3962ac0
Michael Schwarz <m.schwarz@consolinno.de>
2026-03-18 10:19:37 +0100

    ESUI-902. Fix HemsManager initialization for connections that do not require authentication.

--- src/hemsmanager.cpp
+++ src/hemsmanager.cpp
@@ -51,7 +51,7 @@ void HemsManager::setEngine(Engine *engine)
     emit engineChanged();
 
     if (m_engine) {
-        connect(m_engine->jsonRpcClient(), &JsonRpcClient::authenticatedChanged,
+        connect(m_engine->jsonRpcClient(), &JsonRpcClient::connectedChanged,
                 this, &HemsManager::initJsonRpcCommunication);
         initJsonRpcCommunication();
     }
@@ -837,11 +837,12 @@ void HemsManager::factoryResetResponse(int commandId, const QVariantMap &data)
 bool HemsManager::initJsonRpcCommunication()
 {
     qCDebug(dcHems()) << "initJsonRpcCommunication:"
+                        << "host:" << (m_engine->jsonRpcClient()->currentHost() ? m_engine->jsonRpcClient()->currentHost()->name() : QString{ "No host" })
                         << "connected?" << m_engine->jsonRpcClient()->connected()
                         << "connectionStatus:" <<m_engine->jsonRpcClient()->connectionStatus()
                         << "authenticated?" <<m_engine->jsonRpcClient()->authenticated();
 
-    if (!m_engine->jsonRpcClient()->authenticated()) {
+    if (!m_engine->jsonRpcClient()->connected()) {
         qCDebug(dcHems()) << "JsonRpcClient not ready yet";
         m_engine->jsonRpcClient()->unregisterNotificationHandler(this);
         return false;
