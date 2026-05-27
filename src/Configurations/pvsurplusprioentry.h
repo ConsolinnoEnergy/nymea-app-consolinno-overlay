517fe2ea8ca8886ec21f55556e090e08d4d376a1
Michael Schwarz <m.schwarz@consolinno.de>
2026-05-27 13:31:30 +0200

    ESUI-1245. Remove wrong copyright comments.

--- src/Configurations/pvsurplusprioentry.h
+++ src/Configurations/pvsurplusprioentry.h
@@ -1,8 +1,3 @@
-/* Copyright (C) Consolinno Energy GmbH - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
-
 #ifndef PVSURPLUSPRIOENTRY_H
 #define PVSURPLUSPRIOENTRY_H
 
